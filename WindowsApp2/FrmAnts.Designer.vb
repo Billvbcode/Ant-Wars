7c630d08f5541336c05369f5f8c601b3edf7385b
William Perkett <perkettbill@gmail.com>
2021-09-17 11:14:47 -0400

    Add files via upload

---
+++ WindowsApp2/FrmAnts.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmAnts
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmAnts))
+        Me.pAnt1 = New System.Windows.Forms.PictureBox()
+        Me.pAnt2 = New System.Windows.Forms.PictureBox()
+        Me.pAnt3 = New System.Windows.Forms.PictureBox()
+        Me.pAnt4 = New System.Windows.Forms.PictureBox()
+        Me.pAnt5 = New System.Windows.Forms.PictureBox()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.TimMove = New System.Windows.Forms.Timer(Me.components)
+        Me.LblWin = New System.Windows.Forms.Label()
+        CType(Me.pAnt1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pAnt2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pAnt3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pAnt4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pAnt5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pAnt1
+        '
+        Me.pAnt1.Image = CType(resources.GetObject("pAnt1.Image"), System.Drawing.Image)
+        Me.pAnt1.Location = New System.Drawing.Point(44, 289)
+        Me.pAnt1.Name = "pAnt1"
+        Me.pAnt1.Size = New System.Drawing.Size(84, 125)
+        Me.pAnt1.TabIndex = 0
+        Me.pAnt1.TabStop = False
+        '
+        'pAnt2
+        '
+        Me.pAnt2.Image = CType(resources.GetObject("pAnt2.Image"), System.Drawing.Image)
+        Me.pAnt2.Location = New System.Drawing.Point(180, 289)
+        Me.pAnt2.Name = "pAnt2"
+        Me.pAnt2.Size = New System.Drawing.Size(84, 125)
+        Me.pAnt2.TabIndex = 1
+        Me.pAnt2.TabStop = False
+        '
+        'pAnt3
+        '
+        Me.pAnt3.Image = CType(resources.GetObject("pAnt3.Image"), System.Drawing.Image)
+        Me.pAnt3.Location = New System.Drawing.Point(492, 289)
+        Me.pAnt3.Name = "pAnt3"
+        Me.pAnt3.Size = New System.Drawing.Size(84, 125)
+        Me.pAnt3.TabIndex = 2
+        Me.pAnt3.TabStop = False
+        '
+        'pAnt4
+        '
+        Me.pAnt4.Image = CType(resources.GetObject("pAnt4.Image"), System.Drawing.Image)
+        Me.pAnt4.Location = New System.Drawing.Point(322, 289)
+        Me.pAnt4.Name = "pAnt4"
+        Me.pAnt4.Size = New System.Drawing.Size(84, 125)
+        Me.pAnt4.TabIndex = 3
+        Me.pAnt4.TabStop = False
+        '
+        'pAnt5
+        '
+        Me.pAnt5.Image = CType(resources.GetObject("pAnt5.Image"), System.Drawing.Image)
+        Me.pAnt5.Location = New System.Drawing.Point(662, 289)
+        Me.pAnt5.Name = "pAnt5"
+        Me.pAnt5.Size = New System.Drawing.Size(84, 125)
+        Me.pAnt5.TabIndex = 4
+        Me.pAnt5.TabStop = False
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Location = New System.Drawing.Point(416, 424)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(75, 23)
+        Me.BtnStart.TabIndex = 5
+        Me.BtnStart.Text = "Start"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'TimMove
+        '
+        '
+        'LblWin
+        '
+        Me.LblWin.AutoSize = True
+        Me.LblWin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblWin.Location = New System.Drawing.Point(188, 46)
+        Me.LblWin.Name = "LblWin"
+        Me.LblWin.Size = New System.Drawing.Size(63, 20)
+        Me.LblWin.TabIndex = 6
+        Me.LblWin.Text = "Label1"
+        Me.LblWin.Visible = False
+        '
+        'FrmAnts
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LblWin)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Controls.Add(Me.pAnt5)
+        Me.Controls.Add(Me.pAnt4)
+        Me.Controls.Add(Me.pAnt3)
+        Me.Controls.Add(Me.pAnt2)
+        Me.Controls.Add(Me.pAnt1)
+        Me.Name = "FrmAnts"
+        Me.Text = "FrmAnts"
+        CType(Me.pAnt1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pAnt2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pAnt3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pAnt4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pAnt5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pAnt1 As PictureBox
+    Friend WithEvents pAnt2 As PictureBox
+    Friend WithEvents pAnt3 As PictureBox
+    Friend WithEvents pAnt4 As PictureBox
+    Friend WithEvents pAnt5 As PictureBox
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents TimMove As Timer
+    Friend WithEvents LblWin As Label
+End Class
---
+++ WindowsApp2/FrmAnts.vb
@@ -0,0 +1,115 @@
+﻿Public Class FrmAnts
+    Dim iScore As Integer 'To Keep Score
+    Dim iTick As Integer  'To Speed up Timer
+
+
+    Private Sub FrmAnts_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        iScore = 0  ' Reset Score
+        iTick = 0 ' Reset iTick
+        TimMove.Interval = 100  ' Reset Timer      
+        pAnt1.Top = 400 ' Reset Ant1 to the bottom
+        pAnt2.Top = 400 ' Reset Ant2 to the bottom
+        pAnt3.Top = 400 ' Reset Ant3 to the bottom
+        pAnt4.Top = 400 ' Reset Ant4 to the bottom
+        pAnt5.Top = 400 ' Reset Ant5 to the bottom
+        'Show Score
+        Me.Text = "Score=" & iScore & " " _
+            & " Speed =" & TimMove.Interval
+        pAnt4.Visible = False    ' Hide Ant 4
+        pAnt5.Visible = False    ' Hide Ant 5
+        TimMove.Start   ' Start Timer
+        BtnStart.Visible = False ' Hide BtnStart
+        LblWin.Visible = False ' Hide LblWin
+    End Sub
+
+    Private Sub pAnt1_Click(sender As Object, e As EventArgs) Handles pAnt1.Click
+        iScore += 1   ' Increase Score
+        iTick += 1 ' Increase iTick
+        pAnt1.Top = 500 ' Reset Ant1 to the bottom
+        'Show Score
+        Me.Text = "Score=" & iScore & " " _
+            & " Speed =" & TimMove.Interval
+    End Sub
+
+    Private Sub pAnt2_Click(sender As Object, e As EventArgs) Handles pAnt2.Click
+        iScore += 1   ' Increase Score
+        iTick += 1 ' Increase iTick
+        pAnt2.Top = 500 ' Reset Ant2 to the bottom
+        'Show Score
+        Me.Text = "Score=" & iScore & " " _
+            & " Speed =" & TimMove.Interval
+    End Sub
+    Private Sub pAnt3_Click(sender As Object, e As EventArgs) Handles pAnt3.Click
+        iScore += 1   ' Increase Score
+        iTick += 1 ' Increase iTick
+        pAnt3.Top = 500 ' Reset Ant3 to the bottom
+        'Show Score 
+        Me.Text = "Score=" & iScore & " " _
+            & " Speed =" & TimMove.Interval
+    End Sub
+    Private Sub pAnt4_Click(sender As Object, e As EventArgs) Handles pAnt4.Click
+        iScore += 1   ' Increase Score
+        iTick += 1 ' Increase iTick
+        pAnt4.Top = 500 ' Reset Ant4 to the bottom
+        'Show Score
+        Me.Text = "Score=" & iScore & " " _
+            & " Speed =" & TimMove.Interval
+    End Sub
+    Private Sub pAnt5_Click(sender As Object, e As EventArgs) Handles pAnt5.Click
+        iScore += 1   ' Increase Score
+        iTick += 1 ' Increase iTick
+        pAnt5.Top = 500 ' Reset Ant5 to the bottom
+        'Show Score
+        Me.Text = "Score=" & iScore & " " _
+            & " Speed =" & TimMove.Interval
+    End Sub
+
+    Private Sub TimMove_Tick(sender As Object, e As EventArgs) Handles TimMove.Tick
+        If iScore > 5 Then pAnt4.Visible = True 'Ant4 Visible
+        If iScore > 8 Then pAnt5.Visible = True 'Ant5 Visible
+        If iTick > 2 Then 'Speed up Timer
+            TimMove.Interval -= 2 'or 3 or 4 0r 5
+            iTick = 0
+        End If
+        pAnt1.Top -= 5 ' Move ant1 Up
+        If pAnt1.Top < 10 Then 'Ant1 Moves Off Screen
+            TimMove.Stop() 'Stop Timer
+            LblWin.Text = "You Lost " & "Score= " & iScore
+            LblWin.Visible = True 'Show Label
+            BtnStart.Visible = True 'Show Start Button
+        End If
+        pAnt2.Top -= 5 ' Move ant2 Up
+        If pAnt2.Top < 10 Then 'Ant2 Moves Off Screen
+            TimMove.Stop() 'Stop Timer
+            LblWin.Text = "You Lost " & "Score= " & iScore
+            LblWin.Visible = True 'Show Label
+            BtnStart.Visible = True 'Show Start Button
+        End If
+        pAnt3.Top -= 5 ' Move ant3 Up
+        If pAnt3.Top < 10 Then 'Ant3 Moves Off Screen
+            TimMove.Stop() 'Stop Timer
+            LblWin.Text = "You Lost " & "Score= " & iScore
+            LblWin.Visible = True 'Show Label
+            BtnStart.Visible = True 'Show Start Button
+        End If
+        If pAnt4.Visible Then pAnt4.Top -= 5 ' Move ant4 Up
+        If pAnt4.Top < 10 Then 'Ant4 Moves Off Screen
+            TimMove.Stop() 'Stop Timer
+            LblWin.Text = "You Lost " & "Score= " & iScore
+            LblWin.Visible = True 'Show Label
+            BtnStart.Visible = True 'Show Start Button
+        End If
+        If pAnt5.Visible Then pAnt5.Top -= 5 ' Move ant5 Up
+        If pAnt5.Top < 10 Then 'Ant5 Moves Off Screen
+            TimMove.Stop() 'Stop Timer
+            LblWin.Text = "You Lost " & "Score= " & iScore
+            LblWin.Visible = True 'Show Label
+            BtnStart.Visible = True 'Show Start Button
+        End If
+
+    End Sub
+End Class
